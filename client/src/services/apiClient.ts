12e7f711b186d678a99790a9d17b7a6a55fecbbf
Tushar Ravaliya <tushar.ravaliya18@gmail.com>
2026-05-21 14:24:05 +0530

    fix api route

--- client/src/services/apiClient.ts
+++ client/src/services/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { config } from "../config/config.ts";
 
 const api = axios.create({
-  baseURL: config.apiBaseUrl,
+  baseURL: `${config.apiBaseUrl}/api`,
   withCredentials: true,
 });
 
